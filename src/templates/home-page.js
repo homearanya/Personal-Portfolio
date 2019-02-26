a0cf20f9be33d1bde4cd3cc89adf666ae6303fb0
Carlos Gonzalez <homearanya@gmail.com>
2019-02-26 16:25:54 +0200

    rebuild home-pate graphql

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -124,7 +124,7 @@ export const homePageQuery = graphql`
         servicesArea {
           heading
           blurb
-          projects {
+          services {
             iconClasses
             serviceName
             blurb
